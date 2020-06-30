6dd7884b494394f1bbef4f6ba8c85876ed64563b
Simo Aleksandrov <simo3003@me.com>
2020-07-01 00:31:22 +0300

    fix square func

--- gusic/generators.go
+++ gusic/generators.go
@@ -14,9 +14,16 @@ func Square(x float64) float64 {
 	case val > 0:
 		return 1
 	}
+
+	return 1
 }
 
 // Sawtooth is a sawtooth generator function
 func Sawtooth(x float64) float64 {
 	return x - math.Floor(x)
+}
+
+// Triangle is a triangle generator function
+func Triangle(x float64) float64 {
+	return 2 * math.Abs(x/1-math.Floor(x/1+0.5))
 }
