3701b0b9d25906acc763a84023fd582a21c7c4a7
Simo Aleksandrov <simo3003@me.com>
2020-07-05 17:19:45 +0300

    add a check for sum of ratios

--- gusic/ratio.go
+++ gusic/ratio.go
@@ -4,6 +4,10 @@ package gusic
 // Values should be 0.0-1.0 and should sum up to 1.0.
 // They represent a percentage of what duration should each state take.
 func NewRatios(attack, decay, sustain, release float64) ADSRRatios {
+	if attack+decay+sustain+release != 1.0 {
+		panic("gusic: ratios should sum up to 1.0")
+	}
+
 	return ADSRRatios{
 		attack, decay, sustain, release,
 	}
